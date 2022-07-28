1ec6676597c532463e41193dc64344ddc68ffb07
Heapax <mazorsean@gmail.com>
2022-07-28 15:54:42 -0700

    Made changes for working.py and added a requirements.txt file

--- working.py
+++ working.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
 
 @app.get("/")
 def home():
   return {"Data": "Test"}
+
+@app.get('/about')
+def about():
+  return {"Data": "About"}
+
+inventory = {
+    1: {
+      "name": "Milk",
+      "price": 3.99,
+      "brand": "Regular"
+    }
+}
+
+@app.get("/get-item/{item_id}")
+def get_item(item_id: int = Path(None ,description="The ID of the item you would like to view")):
+  return inventory[item_id]
